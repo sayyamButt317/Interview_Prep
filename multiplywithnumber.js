98361ec1e129a57ebfca75eb8a0ddd56e0c46f39
Sayyam <sayyambutt317@gmail.com>
2024-11-19 16:11:49 -0800

    add new question

--- multiplywithnumber.js
+++ multiplywithnumber.js
@@ -1,7 +1,6 @@
 //If we list all the natural numbers below that are multiples of 3 or 5, we get 3,5,6 and 9
 //  The sum of these multiples is Find the sum of all the multiples of or below 1000
 
-<<<<<<< HEAD
 let sum = 0;
 for (let i = 1; i < 1000; i++) {
   if (i % 3 === 0 || i % 5 === 0) {
@@ -9,8 +8,4 @@ for (let i = 1; i < 1000; i++) {
     console.log(sum);
   }
 }
-=======
-const array = [1,34,5,7,8,9,]
-const value =  array.map(arr=>arr*2)
-console.log(value)
->>>>>>> 3c6d444ed5de042900e2b453f2bea69dfcfc15ad
+
